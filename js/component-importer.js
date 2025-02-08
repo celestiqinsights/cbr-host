61b52ae8a65664e19a020f5a0799c8d5092aaa91
celestiqinsights <celestiqinsights@gmail.com>
2025-02-08 19:54:28 +0530

    brands slider

--- js/component-importer.js
+++ js/component-importer.js
@@ -2,4 +2,5 @@ $(document).ready(function () {
   $("#header").load("components/header.html");
   $("#footer").load("components/footer.html");
   $("#category-nav").load("components/category-nav.html");
+  $('#brands-slider').load("components/brands-slider.html");
 });
